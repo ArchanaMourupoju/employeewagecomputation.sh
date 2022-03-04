e8158f00b6bea6d7ef817143aa36e488fd4731c8
ArchanaMourupoju <archanasruthi30@gmail.com>
2022-03-04 13:46:54 +0530

    Added: calculating employee wage using forloop

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,18 +1,36 @@
 #!/bin/bash -x
+function calculateWorkingHour() {
+        case $1 in
+                0)
+                        workingHour=0;
+                        ;;
 
-IS_PRESENT=1;
-IS_PART=2;
-EMP_RATE_PER_HR=20;
-WORKING_DAY_PER_MONTH=20;
-checkEmp=$((RANDOM%3))
+                1)
+                        workingHour=8;
+                        ;;
+                2)
+                        workingHour=4;
+                        ;;
+        esac;
+        echo $workingHour;
+}
 
-case $checkEmp in
-$IS_PRESENT) empHrs=8;
-;;
-$IS_PART) empHrs=4
-;;
-*) empHrs=0
-;;
-esac
-salary=$((empHrs*EMP_RATE_PER_HR))
-echo "Salary=$salary"
+perHourSalary=20;
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+do
+        wHour=$(calculateWorkingHour $((RANDOM%3)));
+        totalWorkingHour=$(($totalWorkingHour + $wHour));
+        if [ $totalWorkingHour -gt 40 ]
+        then
+                totalWorkingHour=$(($totalWorkingHour - $wHour));
+                break;
+        fi
+        salary=$(($perHourSalary * $wHour));
+        totalSalary=$(($totalSalary + $salary));
+        ((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
