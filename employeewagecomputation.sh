a0384b33d6b6dcbda72857ce06298e189ade0aa4
ArchanaMourupoju <archanasruthi30@gmail.com>
2022-03-04 13:26:30 +0530

    Added: calculating wages of month

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,9 +1,11 @@
-#! /bin/bash
+#!/bin/bash -x
 
 IS_PRESENT=1;
 IS_PART=2;
 EMP_RATE_PER_HR=20;
+WORKING_DAY_PER_MONTH=20;
 checkEmp=$((RANDOM%3))
+
 case $checkEmp in
 $IS_PRESENT) empHrs=8;
 ;;
@@ -11,8 +13,6 @@ $IS_PART) empHrs=4
 ;;
 *) empHrs=0
 ;;
-
 esac
-
 salary=$((empHrs*EMP_RATE_PER_HR))
 echo "Salary=$salary"
